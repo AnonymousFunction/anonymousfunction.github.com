b1c3c845e6e3578c0b73559803d7f23c244650b4
Rich McLaughlin <rsmclaug@gmail.com>
2015-01-06 21:02:00 -0500

    more d-pad

--- zelda/zelda.js
+++ zelda/zelda.js
@@ -44,7 +44,36 @@ $(document).ready(function(){
 	updateMapYVal();
 
 	var moveLeft = function(){
+		link.removeClass("up down right").addClass("left");
 
+		if (!canWalkThruWalls() && currentMap[linkY] && currentMap[linkY][linkX-1] === 0) {
+			console.log("link can't move left");
+			return;
+		} else {
+			// console.log("link can move left");
+			linkX--;
+		}
+
+		var originaLeft = parseInt(link.css("left"));
+		var newLeft = originaLeft - 16;
+
+		if (newLeft >= 0) {		
+			link.css("left", newLeft + "px");		
+		} else {
+			var origMapLeft = parseInt(viewport.css("background-position-x"));
+			if (origMapLeft < -256) {
+				var newMapLeft = origMapLeft + 256;
+				viewport.css("background-position-x", newMapLeft + "px");
+
+				var origBeaconLeft = parseInt(beacon.css("left"));
+				var newBeaconLeft = origBeaconLeft - 4;
+				beacon.css("left", newBeaconLeft + "px");
+
+				link.css("left", "240px");
+				linkX = 15;
+				mapX--;
+			}
+		}
 	};
 
 	var moveRight = function(){
@@ -81,138 +110,121 @@ $(document).ready(function(){
 	};
 
 	var moveUp = function(){
+		link.removeClass("down left right").addClass("up");
 
+		if (!canWalkThruWalls() && currentMap[linkY-1] && currentMap[linkY-1][linkX] === 0) {
+			console.log("link can't move up");
+			return;
+		} else if (currentMap[linkY-1] && currentMap[linkY-1][linkX] === 2) {
+			linkY--;
+			console.log("cave");
+		} else {
+			// console.log("link can move up");
+			linkY--;
+		}
+
+		var originalTop = parseInt(link.css("top"));
+		var newTop = originalTop - 16;
+
+		if (newTop >= 56) {
+			link.css("top", newTop + "px");	
+		} else {
+			var origMapTop = parseInt(viewport.css("background-position-y"));
+			if (origMapTop < 0) {
+				var newMapTop = origMapTop + 176;
+				viewport.css("background-position-y", newMapTop + "px");
+
+				var origBeaconTop = parseInt(beacon.css("top"));
+				var newBeaconTop = origBeaconTop - 4;
+				beacon.css("top", newBeaconTop + "px");
+
+				link.css("top", "216px");
+				linkY = 10;
+				mapY--;
+			}
+		}
 	};
 
 	var moveDown = function(){
+		link.removeClass("up left right").addClass("down");
 
+		if (!canWalkThruWalls() && currentMap[linkY+1] && currentMap[linkY+1][linkX] === 0) {
+			console.log("link can't move down");
+			return;
+		} else {
+			// console.log("link can move down");
+			linkY++;
+		}
+
+		var originalTop = parseInt(link.css("top"));
+		var newTop = originalTop + 16;
+
+		if (newTop < 232) {
+			//Moving down one square
+			link.css("top", newTop + "px");
+		} else {
+			//Bottom of map, have to change map
+			var origMapTop = parseInt(viewport.css("background-position-y"));
+
+			if (origMapTop > -1232) {
+				var newMapTop = origMapTop - 176;
+				viewport.css("background-position-y", newMapTop + "px");
+
+				var origBeaconTop = parseInt(beacon.css("top"));
+				var newBeaconTop = origBeaconTop + 4;
+				beacon.css("top", newBeaconTop + "px");
+
+				link.css("top", "56px");
+				linkY = 0;
+				mapY++;
+			}
+		}
 	};
+
+	var doSword = function(){
+		link.addClass("sword");
+		new Audio("sword.wav").play();
+		setTimeout(function(){
+			link.removeClass("sword");
+		}, 200);
+	};
+
+	$("#left").on("click", function(){
+		moveLeft();
+	});
 
 	$("#right").on("click", function(){
 		moveRight();
 	});
 
+	$("#up").on("click", function(){
+		moveUp();
+	});
+
+	$("#down").on("click", function(){
+		moveDown();
+	});
+
 	$("body").keydown(function(e){
-		var isCave = false;
 		//right
 		if (e.which == 39) {
 			moveRight();
 		}
 		//left
 		if (e.which == 37) {
-			link.removeClass("up down right").addClass("left");
-
-			if (!canWalkThruWalls() && currentMap[linkY] && currentMap[linkY][linkX-1] === 0) {
-				console.log("link can't move left");
-				return;
-			} else {
-				// console.log("link can move left");
-				linkX--;
-			}
-
-			var originaLeft = parseInt(link.css("left"));
-			var newLeft = originaLeft - 16;
-
-			if (newLeft >= 0) {		
-				link.css("left", newLeft + "px");		
-			} else {
-				var origMapLeft = parseInt(viewport.css("background-position-x"));
-				if (origMapLeft < -256) {
-					var newMapLeft = origMapLeft + 256;
-					viewport.css("background-position-x", newMapLeft + "px");
-
-					var origBeaconLeft = parseInt(beacon.css("left"));
-					var newBeaconLeft = origBeaconLeft - 4;
-					beacon.css("left", newBeaconLeft + "px");
-
-					link.css("left", "240px");
-					linkX = 15;
-					mapX--;
-				}
-			}
+			moveLeft();
 		}
 		//up
 		if (e.which == 38) {
-			link.removeClass("down left right").addClass("up");
-
-			if (!canWalkThruWalls() && currentMap[linkY-1] && currentMap[linkY-1][linkX] === 0) {
-				console.log("link can't move up");
-				return;
-			} else if (currentMap[linkY-1] && currentMap[linkY-1][linkX] === 2) {
-				linkY--;
-				console.log("cave");
-			} else {
-				// console.log("link can move up");
-				linkY--;
-			}
-
-			var originalTop = parseInt(link.css("top"));
-			var newTop = originalTop - 16;
-
-			if (newTop >= 56) {
-				link.css("top", newTop + "px");
-				if (isCave) {
-					link.addClass("cave");
-				}		
-			} else {
-				var origMapTop = parseInt(viewport.css("background-position-y"));
-				if (origMapTop < 0) {
-					var newMapTop = origMapTop + 176;
-					viewport.css("background-position-y", newMapTop + "px");
-
-					var origBeaconTop = parseInt(beacon.css("top"));
-					var newBeaconTop = origBeaconTop - 4;
-					beacon.css("top", newBeaconTop + "px");
-
-					link.css("top", "216px");
-					linkY = 10;
-					mapY--;
-				}
-			}
+			moveUp();
 		}
 		//down
 		if (e.which == 40) {
-			link.removeClass("up left right").addClass("down");
-
-			if (!canWalkThruWalls() && currentMap[linkY+1] && currentMap[linkY+1][linkX] === 0) {
-				console.log("link can't move down");
-				return;
-			} else {
-				// console.log("link can move down");
-				linkY++;
-			}
-
-			var originalTop = parseInt(link.css("top"));
-			var newTop = originalTop + 16;
-
-			if (newTop < 232) {
-				//Moving down one square
-				link.css("top", newTop + "px");
-			} else {
-				//Bottom of map, have to change map
-				var origMapTop = parseInt(viewport.css("background-position-y"));
-
-				if (origMapTop > -1232) {
-					var newMapTop = origMapTop - 176;
-					viewport.css("background-position-y", newMapTop + "px");
-
-					var origBeaconTop = parseInt(beacon.css("top"));
-					var newBeaconTop = origBeaconTop + 4;
-					beacon.css("top", newBeaconTop + "px");
-
-					link.css("top", "56px");
-					linkY = 0;
-					mapY++;
-				}
-			}
+			moveDown();
 		}
 		//sword
 		if (e.which == 32) {
-			link.addClass("sword");
-			new Audio("sword.wav").play();
-			setTimeout(function(){
-				link.removeClass("sword");
-			}, 200);
+			doSword();
 		}
 
 		updateLinkXVal();
