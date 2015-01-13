4a6de606777b8b6e3a3d8288dda1b290faeb331a
Rich McLaughlin <rsmclaug@gmail.com>
2015-01-13 12:40:27 -0500

    committing map builder

--- zelda/mapBuilder.js
+++ zelda/mapBuilder.js
@@ -1,12 +1,47 @@
 var createMapBuilder = function(){
 	var mapBuilder = $("#map-builder");
 	var mapBuilderHtml = "";
-	for (var x=0; x<16; x++){
-		for (var y=0; y<8; y++) {
-			mapBuilderHtml += "<input type='text' class='mb-input' id='mb" + x + y + "'/>";
+	for (var y=0; y<11; y++){
+        mapBuilderHtml += "<div>";
+		for (var x=0; x<16; x++) {
+            mapBuilderHtml += "<input type='text' class='mb-input' value='0' data-mb-x='" + x + "' data-mb-y='" + y + "'/>";
 		}
-	}
+        mapBuilderHtml += "</div>";
+    }
 	mapBuilder.html(mapBuilderHtml);
-}
+};
 
 createMapBuilder();
+
+var buildMap = function(){
+    var yArray = [];
+    for (var y=0; y<11; y++){
+        var xArray = [];
+        for (var x=0; x<16; x++) {
+            var cellValue = parseInt($("[data-mb-x='" + x + "'][data-mb-y='" + y + "']").val());
+            xArray.push(cellValue);
+        }
+        yArray.push(xArray);
+    }
+
+    var str = "[\n";
+    for (var y=0; y<11; y++){
+        str += "[";
+        for (var x=0; x<16; x++) {
+            if (x == 15) {
+                str += yArray[y][x];
+            } else {
+                str += yArray[y][x] + ",";
+            }
+        }
+        if (y == 10) {
+            str += "]\n];"
+        } else {
+            str += "],\n"
+        }
+    }
+
+    var mapX = $("#mapXVal").text();
+    var mapY = $("#mapYVal").text();
+    console.log("var m" + mapX + mapY + " = ", str);
+};
