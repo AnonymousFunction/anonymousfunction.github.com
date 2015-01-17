2fb137d3922e01dd80e1cdd2bcbc440b07409a2e
Rich McLaughlin <rsmclaug@gmail.com>
2015-01-17 12:57:53 -0500

    trying hammer js for tapping

--- zelda/zelda.js
+++ zelda/zelda.js
@@ -579,15 +579,15 @@ $(document).ready(function(){
     var upInterval;
     var downInterval;
 
-    $.event.special.tap.tapholdThreshold = 100;
+//    $.event.special.tap.tapholdThreshold = 100;
 
-	$("#up").on("vmousedown", function(){
+	$("#up").on("press", function(){
 		moveUp();
         upInterval = $.timer(function(){
             moveUp();
         }, 200);
         upInterval.play();
-	}).on("vmouseup", function(){
+	}).on("pressup", function(){
         upInterval.stop();
         upInterval = undefined;
 	});
