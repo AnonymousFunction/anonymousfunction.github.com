b58795cf83248c991d719299dc0462f6f57e9c00
Rich McLaughlin <rsmclaug@gmail.com>
2015-01-31 12:48:41 -0500

    adding games page

--- controllers/main.js
+++ controllers/main.js
@@ -3,8 +3,7 @@ var af = af || angular.module("af", ["ngRoute"]);
 af.config(function($routeProvider){
 	$routeProvider.
      	when('/', {templateUrl:'views/home.html'}).
-      	when('/code', {templateUrl:'views/code.html'}).
-      	when('/fun', {templateUrl:'views/fun.html'}).
+      	when('/games', {templateUrl:'views/games.html'}).
       	when('/about', {templateUrl:'views/about.html'}).
       	otherwise({redirectTo:'/'});
 });
