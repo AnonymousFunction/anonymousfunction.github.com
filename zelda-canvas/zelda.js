49d5f806fb5c8e11fb937fe7a7e400a6623b91c4
Rich McLaughlin <rsmclaug@gmail.com>
2015-03-02 20:59:22 -0500

    adding sword power

--- zelda-canvas/zelda.js
+++ zelda-canvas/zelda.js
@@ -54,7 +54,7 @@
         // **update()** runs the main game logic.
         update: function () {
             var self = this;
-
+            
             if (this.screenTransitionTime) {
                 if (this.screenTransitionDir === "up") {
                     var origMapTop = parseInt(this.viewport.css("background-position-y"));
@@ -444,6 +444,8 @@
                 this.swordTimer = this.swordCooldown;
                 this.swordWait = this.swordWaitCooldown;
                 this.swordRelease = false;
+                
+                this.game.addBody(new SwordPower(this.game, this.center, this.id));
 
                 Sound.sword.play();
 
@@ -766,6 +768,71 @@
         },
 
         update: function () {
+        }
+    };    
+    
+    var SwordPower = function (game, center, swordDirection) {
+        this.game = game;
+        this.spriteChangeCount = 0;
+        this.spriteCooldown = 6;
+        this.velocity = 3;
+        this.center = { x: center.x, y: center.y };
+        this.direction;
+        
+        if (swordDirection.indexOf("up") > -1) {
+            this.direction = "up";
+            this.id = "sword-power-up";
+            this.size = { x: 7, y: 16 };
+        } else if (swordDirection.indexOf("down") > -1) {
+            this.direction = "down";
+            this.id = "sword-power-down";
+            this.size = { x: 7, y: 16 };
+        } else if (swordDirection.indexOf("left") > -1) {
+            this.direction = "left";
+            this.id = "sword-power-left";
+            this.size = { x: 16, y: 7 };
+        } else if (swordDirection.indexOf("right") > -1) {
+            this.direction = "right";            
+            this.id = "sword-power-right";
+            this.size = { x: 16, y: 7 };
+        }
+        
+    };
+
+    SwordPower.prototype = {
+        draw: function (screen) {
+            var x = this.center.x;
+            var y = this.center.y;
+
+            var img = document.getElementById(this.id);
+            screen.drawImage(img, x - this.size.x / 2, y - this.size.y / 2);
+        },
+
+        update: function () {
+            if (this.spriteChangeCount === 0) {
+                this.spriteChangeCount = this.spriteCooldown;
+            } else {
+                this.spriteChangeCount--;
+            }
+            
+            if (this.direction === "up") {
+                this.id = this.id === "sword-power-up" ? "sword-power-up-flash" : "sword-power-up";
+                this.center.y -= this.velocity;
+            } else if (this.direction === "down") {
+                this.id = this.id === "sword-power-down" ? "sword-power-down-flash" : "sword-power-down";
+                this.center.y += this.velocity;
+            } else if (this.direction === "left") {
+                this.id = this.id === "sword-power-left" ? "sword-power-left-flash" : "sword-power-left";
+                this.center.x -= this.velocity;
+            } else if (this.direction === "right") {
+                this.id = this.id === "sword-power-right" ? "sword-power-right-flash" : "sword-power-right";
+                this.center.x += this.velocity;
+            }
+            
+            if (this.center.x < 0 || this.center.x > 256 || this.center.y < 0 || this.center.y > 176) {
+                this.game.removeBody(this);
+            }
+             
         }
     };
 
