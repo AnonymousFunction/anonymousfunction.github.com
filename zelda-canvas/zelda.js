65f000443ebed11ec952c94d32dab04d537fefbf
Rich McLaughlin <rsmclaug@gmail.com>
2015-03-23 08:25:44 -0400

    starting arrow shooting

--- zelda-canvas/zelda.js
+++ zelda-canvas/zelda.js
@@ -766,6 +766,10 @@
                     if (!this.game.hasBodyByType(CandleFire)) {
                         this.game.addBody(new CandleFire(this.game, this));
                     }
+                } else if (this.equippedItem instanceof EquippedArrow) {
+                    if (!this.game.hasBodyByType(Arrow)) {
+                        this.game.addBody(new Arrow(this.game, this));
+                    }
                 } else if (this.equippedItem instanceof EquippedBomb) {
                     if (!this.game.hasBodyByType(Bomb)) {
                         this.game.addBody(new Bomb(this.game, this));
@@ -980,6 +984,59 @@
         },
 
         update: function () {
+        }
+    };
+
+    var Arrow = function (game, player) {
+        this.game = game;
+        this.velocity = 3;
+        this.center = { x: player.center.x, y: player.center.y };
+        this.direction = player.getDirection();
+
+        switch (this.direction) {
+            case "up":
+                this.id = "arrow-up";
+                this.size = { x: 5, y: 16 };
+                break;
+            case "down":
+                this.id = "arrow-down";
+                this.size = { x: 5, y: 16 };
+                break;
+            case "left":
+                this.id = "arrow-left";
+                this.size = { x: 16, y: 5 };
+                break;
+            case "right":
+                this.id = "arrow-right";
+                this.size = { x: 16, y: 5 };
+                break;
+        }
+    };
+
+    Arrow.prototype = {
+        draw: function (screen) {
+            var x = this.center.x;
+            var y = this.center.y;
+
+            var img = document.getElementById(this.id);
+            screen.drawImage(img, x - this.size.x / 2, y - this.size.y / 2);
+        },
+
+        update: function () {
+            if (this.direction === "up") {
+                this.center.y -= this.velocity;
+            } else if (this.direction === "down") {
+                this.center.y += this.velocity;
+            } else if (this.direction === "left") {
+                this.center.x -= this.velocity;
+            } else if (this.direction === "right") {
+                this.center.x += this.velocity;
+            }
+
+            if (this.center.x < 0 || this.center.x > 256 || this.center.y < 0 || this.center.y > 176) {
+                this.game.removeBody(this);
+            }
+
         }
     };
 
