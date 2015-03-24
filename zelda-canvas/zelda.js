de1e5b43081bc2934cf7d44f94fa25dccc9d5adb
Rich McLaughlin <rsmclaug@gmail.com>
2015-03-24 19:27:51 -0400

    creating bomb explosion

--- zelda-canvas/zelda.js
+++ zelda-canvas/zelda.js
@@ -1043,6 +1043,45 @@
         }
     };
 
+    var createBombExplosion = function(game, center){
+        game.addBody(new Cloud(game, center));
+        game.addBody(new Cloud(game, { x: center.x, y: center.y-10 }));
+        game.addBody(new Cloud(game, { x: center.x-10, y: center.y+10 }));
+        game.addBody(new Cloud(game, { x: center.x+10, y: center.y+10 }));
+    };
+
+    var Cloud = function (game, center) {
+        this.id = "cloud-1";
+        this.game = game;
+        this.size = { x: 16, y: 16 };
+        this.center = { x: center.x, y: center.y };
+        this.countdown = 21;
+    };
+
+    Cloud.prototype = {
+        draw: function (screen) {
+            var x = this.center.x;
+            var y = this.center.y;
+
+            var img = document.getElementById(this.id);
+            screen.drawImage(img, x - this.size.x / 2, y - this.size.y / 2);
+        },
+
+        update: function () {
+            if (this.countdown === 0) {
+                this.game.removeBody(this);
+            } else if (this.countdown <= 7) {
+                this.id = "cloud-3";
+            } else if (this.countdown <= 14) {
+                this.id = "cloud-2";
+            } else if (this.countdown <= 21){
+                this.id = "cloud-1";
+            }
+
+            this.countdown--;
+        }
+    };
+
     var EquippedBomb = function (game, center) {
         this.id = "bomb";
         this.game = game;
@@ -1099,6 +1138,7 @@
             this.explodeCountdown--;
             if (!this.explodeCountdown) {
                 this.game.removeBody(this);
+                createBombExplosion(this.game, this.center);
             }
         }
     };
