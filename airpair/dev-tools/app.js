2cd873080d61ee74570eab191681bdc01157d002
Rich McLaughlin <rsmclaug@gmail.com>
2015-08-16 10:40:51 -0400

    starting page for cdt

--- airpair/dev-tools/app.js
+++ airpair/dev-tools/app.js
@@ -1,16 +1,30 @@
 console.log("Hello World!");
 
-function addNewGame(){
-    var gameTable = document.getElementById("game-table");
+/**
+ * Handles the button click event and resets the input values.
+ */
+function buttonClick(){
     var consoleText = document.getElementById("game-console").value;
     var titleText = document.getElementById("game-title").value;
+
+    addNewGame(consoleText, titleText);
+
+    // Reset the inputs
+    document.getElementById("game-console").value = "";
+    document.getElementById("game-title").value = "";
+}
+
+/**
+ * Creates the HTML for the game and adds it to the table.
+ *
+ * @param consoleText - name of the console, ex: 'NES'
+ * @param titleText - name of the game, ex: 'Super Mario 2'
+ */
+function addNewGame(consoleText, titleText) {
+    var gameTable = document.getElementById("game-table");
 
     var el =  document.createElement("tr");
     el.innerHTML = "<td>" + consoleText + "</td><td>" + titleText + "</td>";
     gameTable.appendChild(el);
+}
 
-    document.getElementById("game-console").value = "";
-    document.getElementById("game-title").value = "";
-
-    console.log("Adding a game");
-};
