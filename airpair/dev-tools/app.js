689a001a94072769722af4baa17cd6e36739373c
Rich McLaughlin <rsmclaug@gmail.com>
2015-08-16 10:34:01 -0400

    starting page for cdt

--- airpair/dev-tools/app.js
+++ airpair/dev-tools/app.js
@@ -1,1 +1,16 @@
 console.log("Hello World!");
+
+function addNewGame(){
+    var gameTable = document.getElementById("game-table");
+    var consoleText = document.getElementById("game-console").value;
+    var titleText = document.getElementById("game-title").value;
+
+    var el =  document.createElement("tr");
+    el.innerHTML = "<td>" + consoleText + "</td><td>" + titleText + "</td>";
+    gameTable.appendChild(el);
+
+    document.getElementById("game-console").value = "";
+    document.getElementById("game-title").value = "";
+
+    console.log("Adding a game");
+};
